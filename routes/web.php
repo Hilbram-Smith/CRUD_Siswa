25dae31a7add4e7afb81aa5ebcfb4e2179c5ff8a
G <sledgeralt@gmail.com>
2026-05-06 13:11:29 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\crudController;
 
-Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
-Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
-Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
-Route::get('/siswa/{id}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
-Route::put('/siswa/{id}', [SiswaController::class, 'update'])->name('siswa.update');
-Route::delete('/siswa/{id}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
+Route::get('/crud', [crudController::class, 'index']);
+route::get('/crud/tambah', [crudController::class, 'tambah']);
+Route::post('/crud/store', [crudController::class, 'store']);
+route::post('/crud/update', [crudController::class, 'update']);
+route::get('/crud/edit/{id}', [crudController::class, 'edit']);
+route::get('/crud/hapus/{id}', [crudController::class, 'hapus']);
